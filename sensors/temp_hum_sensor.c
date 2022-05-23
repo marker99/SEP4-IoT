e7c1372eff5ba74cc93aaa549af8b2e060f3706a
therealRogueWarlock <58672117+therealRogueWarlock@users.noreply.github.com>
2022-05-23 13:22:58 +0200

    Revert "Merge branch 'testBranch' of https://github.com/marker99/SEP4-IoT into testBranch"
    This reverts commit 2652cd5869f29592fba71b494d281c232b411fc2, reversing
    changes made to 1df604e2cc32809292721961654a3a107f7ccae8.

--- sensors/temp_hum_sensor.c
+++ sensors/temp_hum_sensor.c
@@ -6,18 +6,10 @@
 */
 
 #include "temp_hum_sensor.h"
-#include "ApplicationConfig.h"
 
 
 static EventGroupHandle_t _dataReadyEventGroup;
 static EventGroupHandle_t _measureEventGroup;
-
-static TickType_t xLastWakeTime;
-const TickType_t xFrequency = 60000/pdMS_TO_TICKS;
-
-
-static int16_t measuredTemperature;
-static uint16_t measuredHumidity;
 
 void temp_hum_sensor_initialize(UBaseType_t task_priority, EventGroupHandle_t readyGroup, EventGroupHandle_t measureGroup)
 {
@@ -39,63 +31,45 @@ void temp_hum_sensor_initialize(UBaseType_t task_priority, EventGroupHandle_t re
 	NULL);
 }
 
-
 void temp_hum_sensor_task_init(){
 	
 }
 
-
-static void temp_hum_sensor_startMeasure(){
-	
-	xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(50));
+void temp_hum_sensor_task_run(){
 	
 	if (HIH8120_OK != hih8120_wakeup()){
 		printf("temp/hum did not wake up\n");
 	}
 	
-	xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(100));
-
+	// need some delay
 	
 	if (HIH8120_OK != hih8120_measure()){
 		printf("the measurement went wrong\n");
 	}
 	
-	xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(100));
-}
-
-
-void temp_hum_sensor_task_run(){
-	
-	
-	xEventGroupWaitBits(_measureEventGroup,
-	BIT_TEMPHUM_READY_MEASURE,
-	pdFALSE,
-	pdTRUE,
-	portMAX_DELAY
-	);
-	
-	//Sensors start measuring
-	temp_hum_sensor_startMeasure()
-	
+	// need some delay
 	
 	if(hih8120_isReady()){
-		measuredTemperature = hih8120_getTemperature_x10();
-		measuredHumidity = hih8120_getHumidityPercent_x10();
-		
 		// set the ready measurment bit in event group
-		xEventGroupSetBits(_dataReadyEventGroup, BIT_TEMPHUM_READY_MEASURE);
 	}
 	
-	xTaskDelayUntil(&xLastWakeTime, xFrequency);
-	}
+}
 
 
 int16_t temp_hum_getTemperature(void){
-	return measuredTemperature;
+	
+	if (hih8120_isReady()){
+		return hih8120_getTemperature_x10();
+	}
+	return 0;
 }
 
 uint16_t temp_hum_getHumidity(void){
-	return measuredHumidity;
+	// double check, would probably not be necessary when called after event group is set as ready
+	if (hih8120_isReady()){
+		return hih8120_getHumidityPercent_x10();
+	}
+	return 0;
 }
 
 void temp_hum_sensor_task_handler(void *pvParameters)
